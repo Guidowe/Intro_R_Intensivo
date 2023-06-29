cf48dc4816fe6fb25c4deca6a8fdd48f9e649114
unknown <guidowe45@gmail.com>
2023-06-29 11:06:42 -0300

    ordeno

--- Modulo 1 - R Base/Ejercicios Guido.R
+++
@@ -1,6 +0,0 @@
-#Ejercicios clase 1
-base_eph <- read.table(file = "Fuentes/usu_individual_t117.txt",
-                       sep = ";",
-                       header = TRUE,dec = ",")
-mis_variables <- c("CODUSU","P21","CH04","CH06")
-basista <- base_eph[,mis_variables]
